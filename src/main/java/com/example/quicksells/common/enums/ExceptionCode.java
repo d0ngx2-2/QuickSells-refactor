a686154fc67d17bc2cbea9078572d9782dcb1ecc
aram0117 <dkfka568355@gmail.com>
2026-01-14 16:31:01 +0900

    feat: ExceptionCode 추가

--- src/main/java/com/example/quicksells/common/enums/ExceptionCode.java
+++ src/main/java/com/example/quicksells/common/enums/ExceptionCode.java
@@ -10,7 +10,10 @@ public enum ExceptionCode {
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     NOT_FOUND_TOKEN(HttpStatus.NOT_FOUND, "토큰을 찾을 수 없습니다."),
     EXISTS_EMAIL(HttpStatus.CONFLICT, "이미 사용 중인 이메일입니다."),
-    EXISTS_PHONE(HttpStatus.CONFLICT, "이미 존재하는 핸드폰 번호입니다.");
+    EXISTS_PHONE(HttpStatus.CONFLICT, "이미 존재하는 핸드폰 번호입니다."),
+
+    NOT_FOUND_APPRAISE(HttpStatus.NOT_FOUND, "감정을 찾을 수 없습니다."),
+    NOT_FOUND_DEAL(HttpStatus.NOT_FOUND, "거래를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
