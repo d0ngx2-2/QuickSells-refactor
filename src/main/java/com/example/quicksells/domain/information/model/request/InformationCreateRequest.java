2213c519d2f8e483107ed6c1b792e88983f37ab6
최정혁 <jeonghyeogc01@gmaile.com>
2026-01-19 10:29:01 +0900

    refactor: 공지사항 생성 dto 유효성검사 추가

--- src/main/java/com/example/quicksells/domain/information/model/request/InformationCreateRequest.java
+++ src/main/java/com/example/quicksells/domain/information/model/request/InformationCreateRequest.java
@@ -1,6 +1,7 @@
 package com.example.quicksells.domain.information.model.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,12 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class InformationCreateRequest {
 
-    @NotBlank
+    @NotBlank(message = "공지사항 제목은 필수입니다.")
+    @Size(max = 50, message = "공지사항 제목은 최대 50자까지 입력할 수 있습니다.")
     private String title;
 
-    @NotBlank
+    @NotBlank(message = "공지사항 내용은 필수입니다.")
+    @Size(max = 500, message = "공지사항 내용은 최대 500자까지 입력할 수 있습니다.")
     private String description;
 
-    @NotBlank
+    @Size(max = 500, message = "사진 Url은 최대 500자까지 입력할 수 있습니다.")
     private String imageUrl;
 }
