f2d5d783ad1c406af7783ca933bccb2b0f8658f1
aram0117 <dkfka568355@gmail.com>
2026-01-20 12:49:39 +0900

    refactor: Swagger summary 작성 - kar

--- src/main/java/com/example/quicksells/domain/wishlist/controller/WishListController.java
+++ src/main/java/com/example/quicksells/domain/wishlist/controller/WishListController.java
@@ -34,6 +34,7 @@ public class WishListController {
         return ResponseEntity.status(HttpStatus.CREATED).body(CommonResponse.success("관심 목록 등록에 성공했습니다.", result));
     }
 
+    @Operation(summary = "내 관심 목록 조회")
     @GetMapping("/wishList")
     public ResponseEntity<CommonResponse> getAllMyWishList(@AuthenticationPrincipal AuthUser authUser, @RequestParam Long buyerId) {
 
@@ -42,6 +43,7 @@ public class WishListController {
         return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success("내 관심 목록 조회에 성공했습니다.", result));
     }
 
+    @Operation(summary = "내 관심 목록 삭제")
     @DeleteMapping("/wishList")
     public ResponseEntity<CommonResponse> deleteMyWishList(@AuthenticationPrincipal AuthUser authUser, @Valid OneWishListDeleteRequest request) {
 
