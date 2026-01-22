3a7db0703b089d558c1960ed937ba47597fe3158
aram0117 <dkfka568355@gmail.com>
2026-01-22 14:15:01 +0900

    refactor : 안쓰는 jpa 메서드 삭제와 @Parma 삭제 - kar

--- src/main/java/com/example/quicksells/domain/auction/repository/AuctionRepository.java
+++ src/main/java/com/example/quicksells/domain/auction/repository/AuctionRepository.java
@@ -2,11 +2,9 @@ package com.example.quicksells.domain.auction.repository;
 
 import com.example.quicksells.domain.appraise.entity.Appraise;
 import com.example.quicksells.domain.auction.entity.Auction;
-import com.example.quicksells.domain.deal.entity.Deal;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,7 +12,5 @@ public interface AuctionRepository extends JpaRepository<Auction, Long>, Auction
 
     Page<Auction> findAll(Pageable pageable);
 
-    boolean existsByAppraise(@Param("appraiseId") Appraise appraise);
-
-    boolean existsByDeal(@Param("dealId") Deal deal);
+    boolean existsByAppraise(Appraise appraise);
 }
