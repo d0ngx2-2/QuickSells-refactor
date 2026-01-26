d86cab0dc2afebc38db816358d92cd828bdabad5
Tior931108 <jyj8469@gmail.com>
2026-01-26 23:55:28 +0900

    refactor: 경매 조회시 필터링 requestDTO 유효성 검사 추가

--- src/main/java/com/example/quicksells/domain/auction/model/request/AuctionSearchFilterRequest.java
+++ src/main/java/com/example/quicksells/domain/auction/model/request/AuctionSearchFilterRequest.java
@@ -1,5 +1,6 @@
 package com.example.quicksells.domain.auction.model.request;
 
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,9 @@ import lombok.Setter;
 @Setter
 public class AuctionSearchFilterRequest {
 
+    @Min(value = 1, message = "최소 입찰가는 1원 이상이어야 합니다.")
     private Integer minBidPrice; // between 최소 입찰가격
+    @Min(value = 1, message = "최대 입찰가는 1원 이상이어야 합니다.")
     private Integer maxBidPrice; // between 최대 입찰가격
     private String appraiseItemName; // 감정된 아이템 이름
 }
