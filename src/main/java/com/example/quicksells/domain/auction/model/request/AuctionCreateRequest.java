b82d896da833ac9b4eacd74c51c660f5566af151
Tior931108 <jyj8469@gmail.com>
2026-01-27 12:45:28 +0900

    refactor: swagger 요청 DTO @Schema 설명 추가

--- src/main/java/com/example/quicksells/domain/auction/model/request/AuctionCreateRequest.java
+++ src/main/java/com/example/quicksells/domain/auction/model/request/AuctionCreateRequest.java
@@ -1,5 +1,6 @@
 package com.example.quicksells.domain.auction.model.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -8,13 +9,16 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "경매 생성")
 public class AuctionCreateRequest {
 
     @NotNull(message = "감정은 필수입니다.")
+    @Schema(description = "감정 ID")
     private Long appraiseId;
 
     @NotNull(message = "경매종료 시간을 입력해주세요.")
     @Min(value = 1, message = "종료시간은 최소 1일부터 선택가능합니다.")
     @Max(value = 3, message = "종료시간은 최대 3일까지 선택가능합니다.")
+    @Schema(description = "경매 종료 시간 1~3일")
     private int timeOption;
 }
--- src/main/java/com/example/quicksells/domain/information/model/request/InformationCreateRequest.java
+++ src/main/java/com/example/quicksells/domain/information/model/request/InformationCreateRequest.java
@@ -1,5 +1,6 @@
 package com.example.quicksells.domain.information.model.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -9,13 +10,16 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "공지사항 생성")
 public class InformationCreateRequest {
 
     @NotBlank(message = "공지사항 제목은 필수입니다.")
     @Size(max = 50, message = "공지사항 제목은 최대 50자까지 입력할 수 있습니다.")
+    @Schema(description = "공지사항 제목")
     private String title;
 
     @NotBlank(message = "공지사항 내용은 필수입니다.")
     @Size(max = 500, message = "공지사항 내용은 최대 500자까지 입력할 수 있습니다.")
+    @Schema(description = "공지사항 내용")
     private String description;
 }
