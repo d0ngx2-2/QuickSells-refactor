10d8936c3eed4806b455695c1eb94b3b246a2156
aram0117 <dkfka568355@gmail.com>
2026-01-30 11:20:30 +0900

    feat: 경매 입찰 정보 실시간 메시지 개발 - kar
    pub sub 흐름을 보기위한 로그 추가

---
+++ src/main/java/com/example/quicksells/domain/auction/service/AuctionBidSubscriberService.java
@@ -0,0 +1,45 @@
+package com.example.quicksells.domain.auction.service;
+
+import com.example.quicksells.domain.auction.model.dto.BidInfo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RPatternTopic;
+import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class AuctionBidSubscriberService {
+
+    private final RedissonClient redisson;
+    private final SimpMessagingTemplate messagingTemplate;
+    private static final String TOPIC_NAME_PATTERN = "topic:auction:bid:*";
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void setUp() {
+
+        // 래디슨에 저장된 토픽을 패턴으로 찾음
+        RPatternTopic topic = redisson.getPatternTopic(TOPIC_NAME_PATTERN, new JsonJacksonCodec());
+
+        topic.addListener(BidInfo.class, (pattern, channel, msg) -> {
+
+            String buyerName = msg.getBuyerName();
+
+            Integer bidPrice = msg.getBidPrice();
+
+            // 입찰 정보
+            log.info("📥 [SUB] 새로운 입찰 메시지 수신 ▶ Channel : {} ▶ Message : 유저 {} 님이 {}원에 입찰,", channel, buyerName, bidPrice);
+
+            // 웹소켓 목적지 주소 생성 (예: /topic/auction/1)
+            String destination = "/topic/auction/" + msg.getAuctionId();
+
+            // 스톰프로 실시간 입찰 정보 전송 (휘발성)
+            messagingTemplate.convertAndSend(destination, msg);
+        });
+    }
+}
