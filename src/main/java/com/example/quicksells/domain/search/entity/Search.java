36e0b9ed8b874006182545a1afef62537757c512
Leechungwoon <dlcjddns22@gmail.com>
2026-01-30 11:04:57 +0900

    refactor: unique = true 추가,BaseEntity 연결

--- src/main/java/com/example/quicksells/domain/search/entity/Search.java
+++ src/main/java/com/example/quicksells/domain/search/entity/Search.java
@@ -1,27 +1,34 @@
 package com.example.quicksells.domain.search.entity;
 
+import com.example.quicksells.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Table(name = "searches")
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Search {
+public class Search extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    @Column(name = "search_id", nullable = false)
+    @Column(name = "search_id", nullable = false, unique = true)
     private Long id;
 
-    @Column(name = "keyword", length = 50, nullable = false)
+    @Column(name = "keyword", length = 50, nullable = false, unique = true)
     private String keyword;
 
     @Column(name = "count", nullable = false)
     private Long count;
+
 
     public Search(String keyword) {
         this.keyword = keyword;
@@ -31,5 +38,9 @@ public class Search {
     //검색어 증가 (+1)
     public void increase() {
         this.count++;
+    }
+
+    public void updateCount(Long count) {
+        this.count = count;
     }
 }
